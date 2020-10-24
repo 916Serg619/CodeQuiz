a67da5eb4800d42147a48fb25f3e33bf168330f3
Sergio Aguirre <sergioaguirre@Sergios-MBP.attlocal.net>
2020-10-24 16:30:15 -0700

    Final Commit

--- README.md
+++ README.md
@@ -1,91 +1,24 @@
 # 04 Web APIs: Code Quiz
 
-## Your Task
+## Summary
+For this project, I had to create a quiz application with a timer using HTML, CSS, and mostly, Javascript.  
 
-As you proceed in your journey to becoming a full-stack web developer, it’s likely that you’ll be asked to complete a coding assessment, perhaps as part of an interview process. A typical coding assessment is a combination of multiple-choice questions and interactive coding challenges. 
+## How it Works
+Open in Browser. A user has an option to play the game, or view high scores.  Once "Play" is clicked, user will run through a 5 question quiz, and will have to do so in 60 seconds.  The timer starts when the user clicks "Play".  A progress bar on the top left will show which question the user is on.  If the player chooses a wrong choice, the timer will decrease by 5 seconds.  Once the quiz is finish, the user is asked to save their name.  Once saved, the user is directed the "High Scores" page, where they can view the leaderboard.
 
-To help you become familiar with these tests and give you a chance to apply the skills from this module, this week’s homework invites you to build a timed coding quiz with multiple-choice questions. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean, polished, and responsive user interface. This week’s coursework will teach you all the skills you need to succeed in this assignment.
+Screenshots and a movie file are references to how it works. 
 
+## Authors
 
-## User Story
+Sergio Aguirre
+UCSD Bootcamp
 
-```
-AS A coding boot camp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
-```
+## References/Acknowledgments
 
+Youtube.com and W3Schools.com were used as guides to help discover a few commands.  Also used as a reference for other uses of some specific functions.
 
-## Acceptance Criteria
-
-```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and score
-```
+Brian Designs, on Youtube, has a great tutorial that helped me get the skeleton of the code built.    
 
 
-## Mock-Up
-
-The following animation demonstrates the application functionality:
-
-![code quiz](./Assets/04-web-apis-homework-demo.gif)
 
 
-## Grading Requirements
-
-This homework is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality README file with description, screenshot, and link to deployed application.
-
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2020 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- quiz.js
+++ quiz.js
@@ -4,6 +4,38 @@ const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
+/*Variables for timer*/
+const timerCount = document.querySelector('#timer');
+let timerSecond = 60;
+
+/*Display time function*/
+displayTime(timerSecond);
+
+const countDown = setInterval(() => {
+        timerSecond--;
+        displayTime(timerSecond);
+        if (timerSecond <= 0 || timerSecond < 1) {
+            endTime();
+            clearInterval(countDown);
+        }
+
+    }, 1000)
+    /*Ensures that time will display in whole numbers*/
+function displayTime(second) {
+    const min = Math.floor(second / 60);
+    const sec = Math.floor(second % 60);
+    timerCount.innerHTML = `${min<10 ? '0' : ''}${min}:${sec<10?'0':''}${sec}`
+}
+
+
+
+/* Displays message when time is finished, redirects user to finish.html*/
+function endTime() {
+    timerCount.innerHTML = 'TIME!'
+    location.replace('finish.html')
+}
+
+
 let currentQuestion = {}
 let acceptingAnswer = true
 let score = 0
@@ -57,6 +89,7 @@ let questions = [
 const SCORE_POINTS = 100
 const MAX_QUESTIONS = 5
 
+
 startGame = () => {
     questionCounter = 0
     score = 0
@@ -65,34 +98,30 @@ startGame = () => {
 }
 
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
+        if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+            localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('end.html')
+            return window.location.assign('finish.html')
+        }
 
+        questionCounter++
+        progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+        progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+        const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+        currentQuestion = availableQuestions[questionsIndex]
+        question.innerText = currentQuestion.question
+
+        choices.forEach(choice => {
+            const number = choice.dataset['number']
+            choice.innerText = currentQuestion['choice' + number]
+        })
+
+        availableQuestions.splice(questionsIndex, 1)
+
+        acceptingAnswers = true
     }
-
-
-    questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
-
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
-
-    choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText.currentQuestion['choice' + number]
-    })
-
-    availableQuestions.splice(questionsIndex, 1)
-
-    acceptingAnswers = true
-}
-
-
-
+    /*Function for user choices.  Dicates outputs on both correct and incorrect choices*/
 choices.forEach(choice => {
     choice.addEventListener('click', c => {
         if (!acceptingAnswers) return
@@ -101,26 +130,37 @@ choices.forEach(choice => {
         const selectedChoice = c.target
         const selectedAnswer = selectedChoice.dataset['number']
 
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
-            'incorrect'
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
         if (classToApply === 'correct') {
             incrementScore(SCORE_POINTS)
         }
+        /*Else statment that removes time off time if user chooses incorrect choice*/
+        else {
+            timerSecond = timerSecond - 5;
+        }
 
-        selectedChoice.parentElement.classlist.add(classToApply)
+        selectedChoice.parentElement.classList.add(classToApply)
 
         setTimeout(() => {
-            selectedChoiceChoice.parentElement.classlist.remove(classToApply)
+            selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
 
         }, 1000)
     })
 })
 
+
+
+
+
+
 incrementScore = num => {
     score += num
     scoreText.innerText = score
 }
+
+
+
 
 startGame()
